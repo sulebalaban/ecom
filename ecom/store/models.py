4285481739e56d5b1eb456f7a3e3d3087914d45d
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-17 22:21:47 +0100

    feat:Added models

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -4,7 +4,7 @@ import datetime
 class Category(models.Model):
     name=models.CharField(max_length=50)
     
-    def__str__(self):
+    def __str__(self):
         return self.name
 
 class Customer(models.Model):
@@ -12,19 +12,30 @@ class Customer(models.Model):
     last_name=models.CharField(max_length=50)
     phone=models.CharField(max_length=10)
     email=models.EmailField(max_length=50)
-    password=models.PasswordField(max_length=50)
+    password=models.Field(max_length=50)
      
-    def__str__(self):
+    def __str__(self):
         return f'{self.first_name} {self.last_name}'
     
 class Product(models.Model):
     name=models.CharField(max_length=100)
-    price=models.DecimalFieldField(default=10,decimal_places=2,max_digits)
-    category=
-    description=
-    image=
+    price=models.DecimalField(default=10,decimal_places=2,max_digits=6)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    description=models.CharField(max_length=200,default='',blank=True,null=True)
+    image=models.ImageField(upload_to='uploads/product/')
     
-    def__str__(self):
+    def __str__(self):
         return self.name
 
 class Order(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+	quantity = models.IntegerField(default=1)
+	address = models.CharField(max_length=100, default='', blank=True)
+	phone = models.CharField(max_length=20, default='', blank=True)
+	date = models.DateField(default=datetime.datetime.today)
+	status = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.product
+    
