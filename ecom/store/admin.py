0e8d706cc843860a88f207084d85d0444ce35621
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-18 13:24:23 +0100

    feat:Updated static files in home page

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Category,Customer,Product,Order
+from. models import Category,Customer,Product,Order
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
